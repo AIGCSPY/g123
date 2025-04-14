e4900913f9301852b421f0d3c6b20797fb4ca452
xmsumi <275468693@qq.com>
2025-04-14 10:26:04 +0800

    更新readme

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.google.com',
-        pathname: '/s2/favicons/**',
-      },
-    ],
-  },
+  output: 'export'
 };
 
 export default nextConfig;
